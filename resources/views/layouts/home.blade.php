b46ce4ad45eb647f863b11b11e41044d0f9dce54
Muhammet-Sen <temmahumnes@gmail.com>
2020-12-08 17:17:40 +0300

    first commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,55 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Muhammet SEN">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap"
+          rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css" type="text/css">
+
+    @yield('css')
+    @yield('headerjs')
+</head>
+
+<body>
+
+    <div >
+        @include('home._header')
+    </div>
+    <div >
+        @include('home._slider')
+    </div>
+        @section('content')
+            içerik alanı
+        @show
+    <div >
+        @include('home._footer')
+        @yield('footerjs')
+    </div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
